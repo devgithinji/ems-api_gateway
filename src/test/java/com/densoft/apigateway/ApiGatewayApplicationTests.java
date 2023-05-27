2d42052d93c06f36f8554a01c612762a370bf2d1
dennis githinji <wakahiad@gmail.com>
2023-05-27 14:39:30 +0300

    api gateway setup

---
+++ src/test/java/com/densoft/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.densoft.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
